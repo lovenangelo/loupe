61e8dafa4eb0c91a582b87c6eb3580cfcdb22c0e
lovenangelo <lovenangelo.dev@gmail.com>
2026-03-09 05:25:48 +0800

    feat: add reviews in admin

--- reviews/admin.py
+++ reviews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PullRequest, Issue, DraftComment
+
+admin.site.register(PullRequest)
+admin.site.register(Issue)
+admin.site.register(DraftComment)
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return render(request, "reviews/dashboard.html")
+
+
+def store(request):
+    return render("Should save a review")
+
+
+def show(request, review_id):
+    return render("Should get a review")
+
+
+def issues(request):
+    return render("Should show list of issues from review")
